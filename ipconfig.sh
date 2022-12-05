a9ea281ba234757af2407c4d5d8af5cc4a3713c3
lukecobbin <119859826+lukecobbin@users.noreply.github.com>
2022-12-05 16:06:02 +0000

    Update ipconfig.sh

--- ipconfig.sh
+++ ipconfig.sh
@@ -2,14 +2,17 @@
 
 #a bash script for output like ipconfig on windows
 
+#set the interface e.g. eth0
+interface="enp0s3"
+
 printf "IP Address:"
-ip= ifconfig enp0s3 | grep inet | grep -v inet6 | awk '{print $2}'
+ip= ifconfig $interface | grep inet | grep -v inet6 | awk '{print $2}'
 
 printf "Netmask:"
-netmask= ifconfig enp0s3 | grep inet | grep -v inet6 | awk '{print $4}'
+netmask= ifconfig $interface | grep inet | grep -v inet6 | awk '{print $4}'
 
 printf "Broadcast:" 
-broadcast= ifconfig enp0s3 | grep inet | grep -v inet6 | awk '{print $6}'
+broadcast= ifconfig $interface | grep inet | grep -v inet6 | awk '{print $6}'
 
 printf "DNS server:"
 dns= cat /etc/resolv.conf | grep nameserver | awk '{print $2}'
